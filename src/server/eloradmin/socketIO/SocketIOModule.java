46b097341c06f8a4a98b8d606bf2f125404aa84f
Leire Lasa <leire.lasaam@elorrieta-errekamari.com>
2025-01-27 20:00:43 +0100

    Añadir nuevas tablas y atributos

--- src/server/eloradmin/socketIO/SocketIOModule.java
+++ src/server/eloradmin/socketIO/SocketIOModule.java
@@ -58,6 +58,7 @@ public class SocketIOModule {
 		server.addEventListener(Events.ON_LOGOUT.value, MessageInput.class, this.logout());
 		server.addEventListener(Events.ON_RESET_PASS_EMAIL.value, MessageInput.class, this.sendResetPassEmail());
 		server.addEventListener(Events.ON_TEACHER_SCHEDULE.value, MessageInput.class, this.getTeacherSchedule());
+		server.addEventListener(Events.ON_STUDENT_DOCUMENTS.value, MessageInput.class, this.getStudentDocuments());
 	}
 
 	// Default events
@@ -215,6 +216,59 @@ public class SocketIOModule {
 	}
 
 	private DataListener<MessageInput> getTeacherSchedule() {
+		return ((client, data, ackSender) -> {
+			String ip = client.getRemoteAddress().toString();
+			logger.info("[Client = " + ip + "] Client wants to get the schedule");
+			String encryptedMsg = null;
+			try {
+				String clientMsg = data.getMessage();
+				String decryptedMsg = AESUtil.decrypt(clientMsg, key);
+				logger.debug("[Client = " + ip + "] Server received: " + decryptedMsg);
+
+				/*
+				 * Ejemplo de lo que nos llega: { "id": 70, "week": 1}
+				 */
+				Gson gson = new Gson();
+				JsonObject jsonObject = gson.fromJson(decryptedMsg, JsonObject.class);
+				int teacherId = jsonObject.get("id").getAsInt();
+				int selectedWeek = jsonObject.get("week").getAsInt();
+				
+				
+				MessageOutput msgOut = null;
+				if (selectedWeek < 1 || selectedWeek > 39) {
+					msgOut = DefaultMessages.BAD_REQUEST;
+				} else {
+					JsonObject messageObject = new JsonObject();
+					SchedulesManager sm = new SchedulesManager(sesion);
+					ArrayList<TeacherSchedule> schedules = sm.getTeacherWeeklySchedule(teacherId, selectedWeek);
+					
+					if (schedules != null) {
+						JsonArray schedulesArray = new JsonArray();
+						for (TeacherSchedule s : schedules) {
+							JsonObject scheduleJson = gson.toJsonTree(s).getAsJsonObject();
+							schedulesArray.add(scheduleJson);
+						}
+						
+						messageObject.add("schedules", schedulesArray);
+						String messageContent = gson.toJson(messageObject);
+						msgOut = new MessageOutput(HttpURLConnection.HTTP_OK, messageContent);
+					} else {
+						msgOut = DefaultMessages.NOT_FOUND;
+					}
+				}
+				
+				encryptedMsg = AESUtil.encryptObject(msgOut, key);
+				client.sendEvent(Events.ON_TEACHER_SCHEDULE_ANSWER.value, encryptedMsg);
+				logger.debug("[Client = " + ip + "] Sending: " + msgOut.toString());
+			} catch (Exception e) {
+				logger.error("[Client = " + ip + "] Error: " + e.getMessage());
+				encryptedMsg = AESUtil.encryptObject(DefaultMessages.INTERNAL_SERVER, key);
+				client.sendEvent(Events.ON_TEACHER_SCHEDULE_ANSWER.value, encryptedMsg);
+			}
+		});
+	}
+	
+	private DataListener<MessageInput> getStudentDocuments() {
 		return ((client, data, ackSender) -> {
 			String ip = client.getRemoteAddress().toString();
 			logger.info("[Client = " + ip + "] Client wants to get the schedule");
