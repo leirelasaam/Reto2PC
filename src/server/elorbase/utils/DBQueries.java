e94937b0f63543cfe5bed289a566aceb4e266a2d
leirelasaam <leire.lasaam@elorrieta-errekamari.com>
2025-02-02 14:27:47 +0100

    Añadir try&catch en managers

--- src/server/elorbase/utils/DBQueries.java
+++ src/server/elorbase/utils/DBQueries.java
@@ -6,4 +6,5 @@ public class DBQueries {
 	public static final String MEETINGS_BY_TEACHER = "FROM Meeting m JOIN FETCH m.user us JOIN FETCH us.role ro JOIN m.participants p JOIN p.user u JOIN u.role WHERE u.id = :id AND m.week >= :currentWeek ORDER BY m.week ASC, m.day ASC, m.time ASC";
 	public static final String PARTICIPANTS_BY_MEETING = "From Participant p JOIN FETCH p.meeting m JOIN FETCH p.user u JOIN FETCH u.role WHERE p.meeting.id = :id ORDER BY u.lastname ASC, u.name ASC";
 	public static final String ALL_COURSES = "FROM Course";
+	public static final String TEACHER_SCHEDULE_PROCEDURE = "CALL TeacherSchedule(:teacher_id, :selected_week)";
 }
